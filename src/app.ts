0ab634caf2301fc8d45fe225c1a9dc5cf1eab2db
Tanmay Vaij <tanmayvaij22@gmail.com>
2024-06-25 16:41:53 +0530

    added swagger ui
    Signed-off-by: Tanmay Vaij <tanmayvaij22@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,129 @@ import { connectToDatabase } from "./database";
 
 import { checkEnvVariables } from "./utils/checkEnvVariables";
 
+import swaggerJSDoc from "swagger-jsdoc";
+import { serve, setup } from "swagger-ui-express";
+
 config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const options: swaggerJSDoc.Options = {
+  definition: {
+    openapi: "3.1.0",
+
+    basePath: "http://localhost:5000",
+
+    schemes: ["Hello ", "1", "2"],
+
+    tags: [{ name: "Authentication", description: "Sample tag desc" }],
+
+    host: "http://localhost:5000",
+
+    swagger: "aojvoja",
+
+    consumes: ["aojrg", "oav"],
+
+    externalDocs: {
+      url:""
+    },
+
+    produces:["fg", "fg"],
+
+    info: {
+      title: "UAuthX API Docs",
+      version: "1.0.0",
+      contact: {
+        email: "tanmayvaij22@gmail.com",
+        name: "Tanmay Vaij",
+        url: "https://www.github.com/tanmayvaij"
+      },
+      description:
+        "This is an interactive api documentation for uauthx authentication micro-service",
+      license: {
+        name: "MIT",
+        url: "",
+      },
+      termsOfService: "",
+    },
+
+    servers: [
+      {
+        url: "http://localhost:5000",
+        description: "",
+      },
+    ],
+
+    paths: {
+      "/auth/sign-up": {
+        post: {
+          summary: "User sign up",
+          tags: ["Authentication"],
+
+          requestBody: {
+            required: true,
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  required: ["email", "password"],
+                  properties: {
+                    email: {
+                      type: "string",
+                      format: "email",
+                      example: "johndoe@example.com",
+                    },
+                    password: {
+                      type: "string",
+                      format: "password",
+                      example: "Strong_Password@123",
+                    },
+                  },
+                },
+              },
+            },
+          },
+
+          responses: {
+            "200": {
+              description: "",
+              content: {
+                "application/json": {
+                  schema: {
+                    type: "object",
+                    properties: {
+                      isSucess: {
+                        type: "boolean",
+                        example: "true",
+                      },
+                      authToken: {
+                        type: "string",
+                        example: "<your-auth-token>",
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+
+      "/auth/sign-in": {
+        post: {
+          tags: ["Authentication"],
+        },
+      },
+    },
+  },
+
+  apis: [],
+};
+
+app.use("/api-docs", serve, setup(swaggerJSDoc(options)));
 
 app.use("/", rootRouter);
 
