d92ba6f304062be1f880ddbff32c9e85ebd45d49
Tanmay Vaij <tanmayvaij22@gmail.com>
2024-07-03 18:30:30 +0530

    changes in db config and compose yml
    Signed-off-by: Tanmay Vaij <tanmayvaij22@gmail.com>

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,9 +2,7 @@ import { connect } from "mongoose";
 
 export const connectToDatabase = async () => {
   try {
-    await connect(
-      `mongodb://${process.env.UAUTHX_PUBLIC_IP}:${process.env.MONGODB_EXPOSE_PORT}/${process.env.MONGODB_NAME}`
-    );
+    await connect(process.env.MONGODB_URL as string);
     console.log("connected to database successfully");
   } catch (error) {
     console.log(error);
