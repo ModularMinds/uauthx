e6a8fa813e5077ff2c22a0ec6bdd045509a591dd
Tanmay Vaij <tanmayvaij22@gmail.com>
2024-07-03 17:32:32 +0530

    adjustments in env vars
    Signed-off-by: Tanmay Vaij <tanmayvaij22@gmail.com>

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,7 +3,7 @@ import { connect } from "mongoose";
 export const connectToDatabase = async () => {
   try {
     await connect(
-      `${process.env.UAUTHX_PUBLIC_IP}:${process.env.MONGODB_EXPOSE_PORT}/${process.env.MONGODB_NAME}`
+      `mongodb://${process.env.UAUTHX_PUBLIC_IP}:${process.env.MONGODB_EXPOSE_PORT}/${process.env.MONGODB_NAME}`
     );
     console.log("connected to database successfully");
   } catch (error) {
